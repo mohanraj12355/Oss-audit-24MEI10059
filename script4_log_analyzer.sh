bdc51ab6dad6aeb1ddbf27214e6b617466ce36bb
mohanraj12355 <mohanraj.24mei10059@vitbhopal.ac.in>
2026-03-31 20:51:57 +0530

    Update author information in log analyzer script

--- script4_log_analyzer.sh
+++ script4_log_analyzer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 4: Log File Analyzer
-# Author: Rajkumar S (24BCE10447) | Course: Open Source Software
+# Author: mohanraj k(24MEI10059) | Course: Open Source Software
 
 LOGFILE=$1
 KEYWORD=${2:-"error"}   # Default keyword = error
